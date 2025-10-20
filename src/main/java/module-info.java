59187547753cf55de31f353f06fc394bcc3bf63b
Daniel Zapata <zdaniel4771@gmail.com>
2025-10-19 22:51:22 -0700

    SP 1 - moved away from using module-info.java to just the pom.xml

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module csusm.cougarplanner {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.net.http;
-
-    opens csusm.cougarplanner to javafx.fxml;
-    exports csusm.cougarplanner;
-}
